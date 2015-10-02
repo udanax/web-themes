ded659e5ca88cab828baea6856e0875545125a2d
Milos Havlicek <miloshavlicek@gmail.com>
2015-10-02 14:48:44 +0200

    fix

--- web-default/js/recaptcha.js
+++ web-default/js/recaptcha.js
@@ -1,3 +1,24 @@
+function RCgetParameterByName(name) {
+	var scripts = document.getElementsByTagName("script")
+	var params = RCgetParams(scripts[scripts.length - 1].src);
+	return params[name];
+}
+
+function RCgetParams(url) {
+	var regex = /[?&]([^=#]+)=([^&#]*)/g,
+			params = {},
+			match;
+	while (match = regex.exec(url)) {
+		params[match[1]] = match[2];
+	}
+	return params;
+}
+
+var RCLang = (function () {
+	lgParam = RCgetParameterByName("lg");
+	return lgParam === null ? "cs" : lgParam;
+});
+
 function initRecaptcha() {
 	try {
 		var document = window.document;
@@ -13,34 +34,10 @@ function initRecaptcha() {
 		var script = document.createElement('script');
 		script.async = true;
 		script.type = 'text/javascript';
-		script.src = '//www.google.com/recaptcha/api.js?onload=' + callback + '&hl=' + getLang() + '&render=explicit';
+		script.src = '//www.google.com/recaptcha/api.js?onload=' + callback + '&hl=' + RCLang + '&render=explicit';
 		var s = document.getElementsByTagName('script')[0];
 		s.parentNode.insertBefore(script, s);
 	} catch (err) {
 
 	}
 }
-
-function getLang() {
-	lgParam = getParameterByName("lg");
-	return lgParam === null ? "cs" : lgParam;
-}
-
-function getParameterByName(name) {
-	var scripts = document.getElementsByTagName("script")
-	var params = getParams(scripts[scripts.length - 1].src);
-	return params[name];
-}
-
-function getParams(url) {
-	var regex = /[?&]([^=#]+)=([^&#]*)/g,
-			params = {},
-			match;
-	while (match = regex.exec(url)) {
-		params[match[1]] = match[2];
-	}
-	return params;
-}
-
-console.log(getLang());
-console.log(getParameterByName("lg"));
